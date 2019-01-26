c71d96e59618c0a6eb899849d79a5a163204ead9
guang <guang.gg2018@gmail.com>
2019-01-27 01:25:09 +0800

    edit base64 util

--- encryptlib/src/main/java/gapp/season/encryptlib/Base64Util.java
+++ encryptlib/src/main/java/gapp/season/encryptlib/Base64Util.java
@@ -1,4 +1,32 @@
 package gapp.season.encryptlib;
 
+import android.util.Base64;
+
+/**
+ * Base64工具类
+ */
 public class Base64Util {
+    public static byte[] encode(byte[] data) {
+        return Base64.encode(data, Base64.DEFAULT);
+    }
+
+    public static byte[] decode(byte[] base64Data) {
+        return Base64.decode(base64Data, Base64.DEFAULT);
+    }
+
+    public static String encode(String data) {
+        return Base64.encodeToString(data.getBytes(), Base64.DEFAULT);
+    }
+
+    public static String decode(String base64Data) {
+        return new String(Base64.decode(base64Data, Base64.DEFAULT));
+    }
+
+    public static String encodeToString(byte[] data) {
+        return Base64.encodeToString(data, Base64.DEFAULT);
+    }
+
+    public static byte[] decodeString(String base64Data) {
+        return Base64.decode(base64Data, Base64.DEFAULT);
+    }
 }
